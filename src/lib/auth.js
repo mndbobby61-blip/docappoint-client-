32472d569c822428c7acb43e27f7e6bdeb3eacd1
MdBobby <mndbobby61@gmail.com>
2026-05-21 21:09:21 +0600

    env file edit

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,43 +1,43 @@
 import "server-only";
 
 import { betterAuth } from "better-auth";
-import { MongoClient } from "mongodb";
+
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import dns from "node:dns/promises";
 
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
-if (!process.env.MONGODB_URI) {
-    throw new Error("MONGODB_URI missing");
-}
+import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
-// 🔥 MUST connect
 await client.connect();
-console.log("🔥 MongoDB Connected Successfully");
 
 const db = client.db("docappoint");
 
 export const auth = betterAuth({
+
+    secret: process.env.BETTER_AUTH_SECRET,
+
     baseURL: process.env.BETTER_AUTH_URL,
 
     trustedOrigins: [
-        "http://localhost:3001"
+        "http://localhost:3000",
     ],
 
-    database: mongodbAdapter(db, {
-        client,
-    }),
+    database: mongodbAdapter(db),
 
     emailAndPassword: {
         enabled: true,
     },
 
     socialProviders: {
+
         google: {
+
             clientId: process.env.GOOGLE_CLIENT_ID,
+
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
         },
+
     },
+
 });
